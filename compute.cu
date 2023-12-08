7808da6a0f1527a30ea20b3b0777269d6aae8258
janahmedprg <janahmed@ubuntu-2004.eecis.udel.edu>
2023-12-07 21:42:58 -0500

    new

--- compute.cu
+++ compute.cu
@@ -10,13 +10,9 @@
 //Side Effect: Modifies the hPos and hVel arrays with the new positions and accelerations after 1 INTERVAL
 __global__ void compute(double *d_mass, vector3 *d_hPos, vector3 *d_hVel, vector3* d_accels, int d_numObjects, vector3* d_accels_sum){
 	//make an acceleration matrix which is NUMENTITIES squared in size;
-	int blockRow = blockIdx.y;
-	int blockCol = blockIdx.x;
-	int row = threadIdx.y;
-	int col = threadIdx.x;
+	int i = blockIdx.y*blockDim.y + threadIdx.y;
+	int j = blockIdx.x*blockDim.x + threadIdx.x;
 	int k = threadIdx.z;
-
-	printf("%d",blockRow);
 
 	// __shared__ vector3 accelsSub[BLOCK_SIZE * BLOCK_SIZE];
 	// __shared__ vector3 hPosSub[2*BLOCK_SIZE];
@@ -27,24 +23,24 @@ __global__ void compute(double *d_mass, vector3 *d_hPos, vector3 *d_hVel, vector
 	// massSub[col] = d_mass[blockCol + col];	
 	// __syncthreads();
 
-	if (blockRow + row==blockCol + col) {
-		d_accels[(blockRow + row)*d_numObjects + blockCol + col][k] = 0;
+	if (i==j) {
+		d_accels[i*d_numObjects + j][k] = 0;
 	}
 	else{
 		vector3 distance;
-		distance[k]=d_hPos[blockRow + row][k]-d_hPos[blockCol + col][k];
+		distance[k]=d_hPos[i][k]-d_hPos[j][k];
 		double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
 		double magnitude=sqrt(magnitude_sq);
-		double accelmag=-1*GRAV_CONSTANT*d_mass[blockCol + col]/magnitude_sq;
-		d_accels[(blockRow + row)*d_numObjects + blockCol + col][k] = accelmag*distance[k]/magnitude;
+		double accelmag=-1*GRAV_CONSTANT*d_mass[j]/magnitude_sq;
+		d_accels[i*d_numObjects + j][k] = accelmag*distance[k]/magnitude;
 	}
-	d_accels_sum[blockRow + row][k] = 0;
+	d_accels_sum[i][k] = 0;
 	__syncthreads();
 
-	d_accels_sum[blockRow + row][k]+=d_accels[(blockRow + row)*d_numObjects + blockCol + col][k];
+	d_accels_sum[i][k]+=d_accels[i*d_numObjects + j][k];
 	__syncthreads();
-	d_hVel[blockRow + row][k]+=d_accels_sum[blockRow + row][k]*INTERVAL;
-	d_hPos[blockRow + row][k]+=d_hVel[blockRow + row][k]*INTERVAL;
+	d_hVel[i][k]+=d_accels_sum[i][k]*INTERVAL;
+	d_hPos[i][k]+=d_hVel[i][k]*INTERVAL;
 }
 
 // __global__ void sumAccels(vector3 *d_hPos, vector3 *d_hVel, vector3 **d_accels, vector3 *d_accels_sum){
